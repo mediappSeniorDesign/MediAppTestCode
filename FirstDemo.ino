c66320c17db3e39c6f0e2ce848c7adeb2c1f879a
mediappSeniorDesign <gregorylort@gmail.com>
2017-01-10 12:30:42 -0500

    Add files via upload

---
+++ FirstDemo.ino
@@ -0,0 +1,139 @@
+#define IN1  8
+#define IN2  9
+#define IN3  10
+#define IN4  11
+#define IN5 2
+#define IN6 3
+#define IN7 4
+#define IN8 5
+
+int Steps = 0;
+boolean Direction = true;// gre
+unsigned long last_time;
+unsigned long currentMillis ;
+int steps_left=671;
+long time;
+void setup(){
+Serial.begin(9600);
+pinMode(IN1, OUTPUT); 
+pinMode(IN2, OUTPUT); 
+pinMode(IN3, OUTPUT); 
+pinMode(IN4, OUTPUT); 
+pinMode(IN5, OUTPUT);
+pinMode(IN6, INPUT);
+pinMode(IN7, OUTPUT);
+pinMode(IN8, INPUT);
+digitalWrite(IN5, LOW);
+digitalWrite(IN7, LOW);
+
+// delay(1000);
+}
+
+void loop(){
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(IN5, HIGH);
+  digitalWrite(IN7, HIGH);
+  if(digitalRead(IN6) == LOW){
+    turnOne(1);
+    delay(2000);
+    turnOne(0);
+    delay(2000);
+  }
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+  digitalWrite(LED_BUILTIN, HIGH);
+  if(digitalRead(IN8) == LOW){
+    turnOne(1);
+    turnOne(1);
+    delay(2000);
+    turnOne(0);
+    turnOne(0);
+    delay(2000); 
+  }
+  digitalWrite(LED_BUILTIN, LOW);
+
+}
+
+void turnOne(bool dir){
+  Direction = dir;
+  while(steps_left>0){
+  currentMillis = micros();
+    if(currentMillis-last_time>=1000){
+      stepper(1); 
+      time=time+micros()-last_time;
+      last_time=micros();
+      steps_left--;
+    }
+  }
+  steps_left=671; 
+
+}
+
+void stepper(int xw){
+  for (int x=0;x<xw;x++){
+    switch(Steps){
+       case 0:
+         digitalWrite(IN1, LOW); 
+         digitalWrite(IN2, LOW);
+         digitalWrite(IN3, LOW);
+         digitalWrite(IN4, HIGH);
+       break; 
+       case 1:
+         digitalWrite(IN1, LOW); 
+         digitalWrite(IN2, LOW);
+         digitalWrite(IN3, HIGH);
+         digitalWrite(IN4, HIGH);
+       break; 
+       case 2:
+         digitalWrite(IN1, LOW); 
+         digitalWrite(IN2, LOW);
+         digitalWrite(IN3, HIGH);
+         digitalWrite(IN4, LOW);
+       break; 
+       case 3:
+         digitalWrite(IN1, LOW); 
+         digitalWrite(IN2, HIGH);
+         digitalWrite(IN3, HIGH);
+         digitalWrite(IN4, LOW);
+       break; 
+       case 4:
+         digitalWrite(IN1, LOW); 
+         digitalWrite(IN2, HIGH);
+         digitalWrite(IN3, LOW);
+         digitalWrite(IN4, LOW);
+       break; 
+       case 5:
+         digitalWrite(IN1, HIGH); 
+         digitalWrite(IN2, HIGH);
+         digitalWrite(IN3, LOW);
+         digitalWrite(IN4, LOW);
+       break; 
+         case 6:
+         digitalWrite(IN1, HIGH); 
+         digitalWrite(IN2, LOW);
+         digitalWrite(IN3, LOW);
+         digitalWrite(IN4, LOW);
+       break; 
+       case 7:
+         digitalWrite(IN1, HIGH); 
+         digitalWrite(IN2, LOW);
+         digitalWrite(IN3, LOW);
+         digitalWrite(IN4, HIGH);
+       break; 
+       default:
+         digitalWrite(IN1, LOW); 
+         digitalWrite(IN2, LOW);
+         digitalWrite(IN3, LOW);
+         digitalWrite(IN4, LOW);
+       break; 
+    }
+  SetDirection();
+  }
+} 
+void SetDirection(){
+if(Direction==1){ Steps++;}
+if(Direction==0){ Steps--; }
+if(Steps>7){Steps=0;}
+if(Steps<0){Steps=7; }
+}
+
---
+++ PillsFirstSketch1.ino
@@ -0,0 +1,69 @@
+
+//set up pin ariables
+int disk = 0;
+int compartment = 0;
+int input = 0;
+int ticker = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+    //primary motor 
+    pinMode(motor1Pin1, OUTPUT);
+    pinMode(motor1Pin2, OUTPUT);
+    pinMode(enable1Pin, OUTPUT);
+    //sleeve motor
+    pinMode(motor2Pin1, OUTPUT);
+    pinMode(motor2Pin2, OUTPUT);
+    pinMode(enable2Pin, OUTPUT);
+    //ticker sensor
+    pinMode(tickerPin, INPUT);
+    //enable both
+    digitalWrite(enable1Pin, HIGH);
+    digitalWrite(enable2Pin, HIGH);
+    
+    // initialize serial communication at 9600 bits per second:
+    Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    if ( Serial.available() > 0){
+      input = Serial.read();
+      disk = input/10;
+      compartment = input%10;
+
+      //shift slider to appropriate disk
+
+      //rotate shaft appropriate number of ticks
+      int temp = compartment;
+      while (temp > 0){
+        digitalWrite(motor1pin1, HIGH);
+        digitalWrite(motor1pin2, LOW);
+        ticker = 0;
+        while(ticker == 0){
+          ticker = digitalRead(tickerpin);
+        }
+        temp--;
+      }
+      digitalWrite(motor1pin1, LOW);
+      digitalWrite(motor1pin2, LOW);
+      //rotate back
+      temp = compartment;
+      while (temp > 0){
+        digitalWrite(motor1pin1, LOW);
+        digitalWrite(motor1pin2, HIGH);
+        ticker = 0;
+        while(ticker == 0){
+          ticker = digitalRead(tickerpin);
+        }
+        temp--;
+      }
+      digitalWrite(motor1pin1, LOW);
+      digitalWrite(motor1pin2, LOW);
+
+      //return slider to rest point
+    }
+
+    
+    
+}
