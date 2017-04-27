e392308767e1331978e86e7bca606c285f45a997
mediappSeniorDesign <gregorylort@gmail.com>
2017-04-27 18:02:24 -0400

    Add files via upload

---
+++ Alarm_Buzzer.ino
@@ -0,0 +1,23 @@
+
+const int pushButton = 3;
+int Buzzer1 = 12;
+int buttonState = 0;
+
+void setup() {
+  pinMode(Buzzer1, OUTPUT);
+  pinMode(pushButton, INPUT);
+  pinMode(2, OUTPUT);
+  digitalWrite(2, HIGH);
+}
+
+void loop() {
+  buttonState = digitalRead(pushButton);
+  if(buttonState == LOW) {
+      tone(Buzzer1,400,200);
+      delay(500);  
+  }
+  
+}
+
+
+
